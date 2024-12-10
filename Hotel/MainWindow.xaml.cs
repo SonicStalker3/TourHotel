3b5fab4f3ae11cf22666698ebe03a10ff95ffebf
SonicStalker3 <52555864+SonicStalker3@users.noreply.github.com>
2024-12-10 17:03:24 +0800

    Начало!

---
+++ Hotel/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Windows;
+//using 
+
+namespace HotelApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
